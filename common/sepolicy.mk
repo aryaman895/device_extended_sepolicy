739962e6a64e0cdef79c4984d24f8a1c2287c02a
shivatejapeddi <shivatejapeddi@gmail.com>
2020-12-29 13:54:10 +0530

    sepolicy : Aosp >> Extended

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -10,16 +10,16 @@ endif
 endif
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/aosp/sepolicy/common/public
+    device/extended/sepolicy/common/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/aosp/sepolicy/common/private
+    device/extended/sepolicy/common/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/aosp/sepolicy/common/dynamic
+    device/extended/sepolicy/common/dynamic
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/aosp/sepolicy/common/dynamic \
-    device/aosp/sepolicy/common/vendor
+    device/extended/sepolicy/common/dynamic \
+    device/extended/sepolicy/common/vendor
 endif
